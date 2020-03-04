b598086fb22b00d7bfc5031ecc9c097f37654ffe
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-03-04 14:48:25 -0800

    set up login page view/card

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navigation from './components/Navigation'
 
+// Pages
+import Login from './pages/Login';
+
 class App extends React.Component {
-  render() {
-    return (
-      <Navigation />
-    )
-  }
+    render() {
+        return (
+            <Router>
+                <Navigation />
+                <Switch>
+                    <Route path='/' exact component={Login} />
+                </Switch>
+            </Router>
+        )
+    }
 }
 
 export default App;
---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    Card, Button, CardText, CardBody, CardLink,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
+import './style.css';
+import logo from '../../PERSON.png'
+
+function Login() {
+    return (
+        <div id="login" className="container">
+            <Card body id="log-card" className="text-center text-light">
+                <CardText><img src={logo}/></CardText>
+                <CardTitle tag="h1">Commit to be Fit.</CardTitle>
+                <Button className="btn-danger">Sign Up</Button>
+            </Card>
+        </div>
+    );
+}
+
+export default Login;
